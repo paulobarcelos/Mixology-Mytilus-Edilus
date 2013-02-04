2a4102307b2b7102bc80a934b5b4bfb5e0d1f51e
Paulo Barcelos <paulo@azucrina.org>
2013-02-04 23:14:32 +0100

    Changes on index

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var static = require('node-static');
-var file = new static.Server('./public');
+var file = new static.Server('./public', {cache:0});
 require('http').createServer(function (request, response) {
 	request.addListener('end', function () {
 		file.serve(request, response);
